e865add90f0d2f58921b84c0ee049ff423648eb5
fcrow712 <frank@arisoft.com.mx>
2018-08-09 12:09:00 -0600

    inicio webservice

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+const jwt = require('jsonwebtoken');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+server.use(cors({ credentials: true, origin: true }));
+//settings
+const port = process.env.PORT || 3001;
+server.set('port', process.env.PORT || 3001);
+//server.set('secret', config.secret);
+//middleware
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+// Rutas
+//require('./routes/authenticate')(server);
+
+/*//Middleware to check whether you're auth - Todas las rutas arriba estan desprotegidas
+server.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, server.get('secret'), function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Falló la autenticación del token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an error
+        return res.status(405).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+});
+//// Middleware token - Todas las rutas bajo esto estaran protegidas \\\\
+//require('./routes/articulo')(server);*/
+
+server.listen(server.get('port'), () => {
+    console.log("Started successfuly in the port 3001");
+});
